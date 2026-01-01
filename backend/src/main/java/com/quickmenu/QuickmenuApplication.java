c380e2bb7d53ff50203b93ea3e2961c219baedb1
Sohail Ahmad <67598673+sohail24@users.noreply.github.com>
2026-01-01 19:18:52 +0530

    feat(add): enable cron for demo data restoration

--- backend/src/main/java/com/quickmenu/QuickmenuApplication.java
+++ backend/src/main/java/com/quickmenu/QuickmenuApplication.java
@@ -2,8 +2,10 @@ package com.quickmenu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 public class QuickmenuApplication {
 
 	public static void main(String[] args) {
